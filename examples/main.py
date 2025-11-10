01f0a55fe525c4b1d6d48e5d704b7a0b869a19ce
aguang5241 <gliu4@wpi.edu>
2025-11-10 12:11:11 -0500

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	new file:   examples/main.py
    	new file:   examples/test/a
    	new file:   examples/test/b
    	new file:   examples/test/c
    	new file:   examples/test/d
    	new file:   examples/test/e
    	new file:   examples/test/f
    	new file:   examples/tools.py

---
+++ examples/main.py
@@ -0,0 +1,28 @@
+#### main.py
+from pydantic_ai.models.openai import OpenAIChatModel
+from pydantic_ai import Agent
+
+from dotenv import load_dotenv
+import tools
+
+load_dotenv()
+model = OpenAIChatModel(model_name='gpt-5-nano')
+
+agent = Agent(
+    model=model,
+    system_prompt='You are a experienced programmer',
+    tools=[tools.read_file, tools.list_files, tools.rename_file],
+    )
+
+def main():
+    history = []
+    while True:
+        user_input = input("Input: ")
+        resp = agent.run_sync(user_prompt=user_input,
+                              message_history=history)
+        history = list(resp.all_messages())
+        print(resp.output)
+
+
+if __name__ == "__main__":
+    main()
